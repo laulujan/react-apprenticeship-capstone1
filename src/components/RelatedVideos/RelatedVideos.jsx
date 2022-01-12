bf8ddfefcd1e2f0d642934f5c0706ed0ae518184
Laura Lujan <laura.lujan@wizeline.com>
2022-01-12 16:27:00 -0600

    feat: add click event to related videos items

--- src/components/RelatedVideos/RelatedVideos.jsx
+++ src/components/RelatedVideos/RelatedVideos.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import {
   RelatedContent,
   VideoItem,
   Image,
   Title,
 } from './RelatedVideos.styled';
+import { useVideo } from '../../providers/Video/Video.provider';
 
 const RelatedVideos = ({ videosList }) => {
+  const { setCurrentVideo } = useVideo();
+  const history = useHistory();
+
+  const handleClick = (video) => {
+    setCurrentVideo(video);
+    history.push(`/video/${video.id}`);
+  };
   return (
     <RelatedContent>
       {videosList.map((video) => (
-        <VideoItem key={video.id}>
+        <VideoItem key={video.id} onClick={() => handleClick(video)}>
           <Image src={video.image} alt={video.title} />
           <Title>{video.title}</Title>
         </VideoItem>
